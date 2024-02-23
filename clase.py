d6c16f0b8efa417149b237a3fbce45720c805641
Daniel-Maza29 <dnlmz31393827@gmail.com>
2024-02-23 08:28:29 -0500

    mi primer commit

---
+++ clase.py
@@ -0,0 +1,11 @@
+class persona:
+    def __init__(self,nombre,apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+        
+    def Obtenernombre(self):
+             return f'MI nombre es {self.nombre} {self.apellido}'
+         
+         #instanciamos la clase 
+               
+ 
